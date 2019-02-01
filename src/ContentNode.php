b3d250aa6590dec5f9b17a8c34f018a4cb6b6239
Thomas Koudela <thomas@koudela.net>
2019-02-01 12:30:37 +0100

    hello: 2019

--- src/ContentNode.php
+++ src/ContentNode.php
@@ -1,12 +1,13 @@
 <?php
 /**
-* e-Arc Framework - the explicit Architecture Framework
-*
-* @package earc/tree
-* @link https://github.com/Koudela/eArc-tree/
-* @copyright Copyright (c) 2018 Thomas Koudela
-* @license http://opensource.org/licenses/MIT MIT License
-*/
+ * e-Arc Framework - the explicit Architecture Framework
+ * tree composite component
+ *
+ * @package earc/tree
+ * @link https://github.com/Koudela/eArc-tree/
+ * @copyright Copyright (c) 2018-2019 Thomas Koudela
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
 
 namespace eArc\Tree;
 
--- src/Exceptions/NotPartOfTreeException.php
+++ src/Exceptions/NotPartOfTreeException.php
@@ -1,10 +1,11 @@
 <?php
 /**
  * e-Arc Framework - the explicit Architecture Framework
+ * tree composite component
  *
  * @package earc/tree
  * @link https://github.com/Koudela/eArc-tree/
- * @copyright Copyright (c) 2018 Thomas Koudela
+ * @copyright Copyright (c) 2018-2019 Thomas Koudela
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
